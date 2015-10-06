5f0380ee02eb8c63cc76cd1edb9083cb310bddee
Viktor Danilov <duke.img@gmail.com>
2015-10-06 20:33:53 +0300

    Drag & Drop

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require jquery-ui/draggable
-//= require jquery-ui/droppable
-//= require jquery-ui/resizable
-//= require jquery-ui/selectable
-//= require jquery-ui/sortable
+//= require jquery-ui
 //= require_tree .
--- app/assets/javascripts/films.js
+++ app/assets/javascripts/films.js
@@ -15,25 +15,6 @@ var i = 1;
 loading = function() {
     console.log(i);
 
-    sound_click.setAttribute('src', 'https://dl.dropboxusercontent.com/u/100656262/round_pop_click1.wav');
-    sound_drop.setAttribute('src', 'https://dl.dropboxusercontent.com/u/100656262/round_pop_click1.wav');
-    sound_drop_back.setAttribute('src', 'https://dl.dropboxusercontent.com/u/100656262/round_pop_click1.wav');
-    //sound_click.setAttribute('autoplay', 'autoplay');
-    //sound_click.load()
-
-    $.get();
-
-    sound_click.addEventListener("load", function() {
-        sound_click.play();
-    }, true);
-    sound_drop.addEventListener("load", function() {
-        sound_click.play();
-    }, true);
-    sound_drop_back.addEventListener("load", function() {
-        sound_click.play();
-    }, true);
-
-
     switch (i) {
         case 1:
             $(".color-3").css("left", "0%");
@@ -71,12 +52,37 @@ loading = function() {
     }, 3000);
 };
 
+function mediaLoad() {
+
+    sound_click.setAttribute('src', 'https://dl.dropboxusercontent.com/u/100656262/round_pop_click.wav');
+    sound_drop.setAttribute('src', 'https://dl.dropboxusercontent.com/u/100656262/round_pop_click1.wav');
+    sound_drop_back.setAttribute('src', 'https://dl.dropboxusercontent.com/u/100656262/round_pop_click2.wav');
+    //sound_click.setAttribute('autoplay', 'autoplay');
+    //sound_click.load()
+
+    $.get();
+
+    sound_click.addEventListener("load", function() {
+        sound_click.play();
+    }, true);
+    sound_drop.addEventListener("load", function() {
+        sound_drop.play();
+    }, true);
+    sound_drop_back.addEventListener("load", function() {
+        sound_click.play();
+    }, true);
+
+}
+
 $(document).ready(function(){
     a = true;
+    var drag;
+
     $('.tool').mousedown(function(e){
+        drag = true;
         console.log('yay!');
         sound_click.play();
-        $("<div id='object'>hello world</div>").appendTo('.page').offset({left:e.pageX,top:e.pageY});
+        $("<div id='object'><span style='display:block'>hello world</span></div>").appendTo('.page').offset({left:e.pageX,top:e.pageY}).draggable();
         $(document).on('mousemove', function(e){
             $('#object').css({
                 left:  e.pageX,
@@ -84,10 +90,42 @@ $(document).ready(function(){
             });
             $(document).mouseup(function() {
                 $(document).off("mousemove");
+                if (drag == true) {
+                    sound_drop.play();
+                }
+                drag = false;
             });
         });
     });
+
+    var measurements;
+    $(document).on({
+        mouseenter: function () {
+            measurements = [$(this).width(), $(this).height(), $(this).offset().top, $(this).offset().left ];
+            console.log("width: " + $(this).width());
+            var w = $(this).width() * 1.25;
+            console.log("w: " + w);
+            $(this).width(w);
+            $(this).height(w);
+            $(this).css({ top: ($(this).offset().top - ((w - measurements[1]) / 2))});
+            $(this).css({ left: $(this).offset().left - ((w - measurements[0]) / 2) });
+            console.log((w - measurements[0]) / 2);
+            $(this).mouseup(function() {
+                sound_drop.play();
+                measurements[2] = $(this).offset().top + ((w - measurements[1]) / 2);
+                measurements[3] = $(this).offset().left + ((w - measurements[0]) / 2);
+            });
+
+        },
+        mouseleave: function () {
+            $(this).width(measurements[0] + 4);
+            $(this).height(measurements[1] + 4);
+            $(this).css({ top: measurements[2] });
+            $(this).css({ left: measurements[3] });
+        }
+    }, "#object");
 });
+
 
 
 
