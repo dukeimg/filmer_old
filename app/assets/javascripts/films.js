1a201e2f67b0337bdf617067f2130e39c0e293b1
Viktor Danilov <duke.img@gmail.com>
2015-10-09 12:25:31 +0300

    Still refactoring JS

--- app/assets/javascripts/films.js
+++ app/assets/javascripts/films.js
@@ -1,18 +1,22 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+//VARIABLES
+
 var a = false;
 var sound_click = document.createElement('audio');
 var sound_drop = document.createElement('audio');
 var sound_drop_back = document.createElement('audio');
+var i = 1;
+var objID = 1;
 
    // $(window).load(function() {
      //   a = true;
     //});
 
-var loading;
-var i = 1;
-loading = function() {
+//FUNCTIONS
+
+function loading() {
     console.log(i);
 
     switch (i) {
@@ -50,7 +54,7 @@ loading = function() {
             })
         }
     }, 3000);
-};
+}
 
 function mediaLoad() {
 
@@ -74,15 +78,58 @@ function mediaLoad() {
 
 }
 
-$(document).ready(function(){
+function objectCreate(type) {
+    // get ID
+    if ($('object-' + objID).length) {
+        objID++;
+    }
+    drag = true; // start of dragging
+    sound_click.play();
+
+    // get object type
+
+   /* switch(type) {
+        case 'label'
+    }*/
+
+    /*$("<div id='object'><span id='obj-label' style='display:block'></span></div>").appendTo('.page').offset({left:e.pageX,top:e.pageY}).draggable(); // create an object
+    measurements = [$('#object').width(), $('#object').height(), $('#object').offset().top, $('#object').offset().left ];
+    var w = $('#object').width() * 1.25;
+    $('#object').width(w).height(w).css({ top: ($(this).offset().top - ((w - measurements[1]) / 2))}).css({ left: $(this).offset().left - ((w - measurements[0]) / 2) });
+    $(document).on('mousemove', function(e){
+        $('#object').css({
+            left:  e.pageX,
+            top:   e.pageY
+        });
+        $(document).mouseup(function() {
+            $(document).off("mousemove");
+            if (drag == true) {
+                sound_drop.play();
+            }
+            drag = false; // finish of dragging
+        });
+    });*/
+}
+
+//OBJECTS
+//nothing is here for now
+
+/*$(document).ready(function(){
     a = true;
     var drag;
+    var measurements;
+    var font = 14;
 
     $('.tool').mousedown(function(e){
         drag = true;
-        console.log('yay!');
         sound_click.play();
-        $("<div id='object'><span style='display:block'>hello world</span></div>").appendTo('.page').offset({left:e.pageX,top:e.pageY}).draggable();
+        $("<div id='object'><span id='obj-label' style='display:block'>hello world</span></div>").appendTo('.page').offset({left:e.pageX,top:e.pageY}).draggable();
+        measurements = [$('#object').width(), $('#object').height(), $('#object').offset().top, $('#object').offset().left ];
+        var w = $('#object').width() * 1.25;
+        $('#object').width(w);
+        $('#object').height(w);
+        $('#object').css({ top: ($('#object').offset().top - ((w - measurements[1]) / 2))});
+        $('#object').css({ left: $('#object').offset().left - ((w - measurements[0]) / 2) });
         $(document).on('mousemove', function(e){
             $('#object').css({
                 left:  e.pageX,
@@ -97,33 +144,46 @@ $(document).ready(function(){
             });
         });
     });
-
-    var measurements;
+    var lastObject;
     $(document).on({
-        mouseenter: function () {
-            measurements = [$(this).width(), $(this).height(), $(this).offset().top, $(this).offset().left ];
-            console.log("width: " + $(this).width());
-            var w = $(this).width() * 1.25;
-            console.log("w: " + w);
-            $(this).width(w);
-            $(this).height(w);
-            $(this).css({ top: ($(this).offset().top - ((w - measurements[1]) / 2))});
-            $(this).css({ left: $(this).offset().left - ((w - measurements[0]) / 2) });
-            console.log((w - measurements[0]) / 2);
-            $(this).mouseup(function() {
-                sound_drop.play();
-                measurements[2] = $(this).offset().top + ((w - measurements[1]) / 2);
-                measurements[3] = $(this).offset().left + ((w - measurements[0]) / 2);
-            });
+        click: function(){
+            $(this).draggable('enable').resizable(
+                {   disabled: false,
+                    aspectRatio: true,
+                    start: (function(){$(this).toggleClass('on-resize')}),
+                    stop: (function(){$(this).toggleClass('on-resize')}),
+                    resize: (function(){
 
-        },
-        mouseleave: function () {
-            $(this).width(measurements[0] + 4);
-            $(this).height(measurements[1] + 4);
-            $(this).css({ top: measurements[2] });
-            $(this).css({ left: measurements[3] });
+                        //Get window width
+                        var width = $("#obj-label").width();
+                        var width2 = $('#object').width() * 0.75;
+
+                        //Set new font size
+                        var newFont = font * (width2/width);
+                        $('#object').css('font-size', Math.floor(newFont));
+                        font = newFont;
+                    })
+                }
+            );
+            lastObject = $(this);
+            $(this).addClass('active');
+            console.log("enabled");
         }
     }, "#object");
+
+    $('.page').on({
+        click: function(){
+            $(lastObject).draggable('disable').resizable({disabled: true});
+            console.log("disabled");
+            $(lastObject).removeClass('active');
+        }
+    }, !lastObject);
+});*/
+
+//READY
+
+$(document).ready(function(){
+
 });
 
 
