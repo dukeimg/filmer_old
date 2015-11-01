b298fc07fa006d2e765bb1f99f48557fdce4bf41
dukeimg <duke.img@gmail.com>
2015-11-01 18:47:21 +0300

    The work is in progress agian!!! Yay!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   layout "pages"
+  layout "landing", only: [:index]
 
   def index
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :films
   get 'login' => 'sessions#new'
   get 'projects' => 'projects#index'
+  get '/videoplayback', :to => redirect('/videoplayback.mp4')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
