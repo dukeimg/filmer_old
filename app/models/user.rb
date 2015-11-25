4c6b98178a58b95c0636fef334b7e0fa0f701668
dukeimg <duke.img@gmail.com>
2015-11-25 22:29:07 +0300

    Crop

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,18 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :avatar, styles: { medium: "250x250#", thumb: "50x50#" }, default_url: "/images/:style/missing.png", :processors => [:cropper]
+  has_many :suggestions
+  has_attached_file :avatar, styles: {preview: '600x600>',  medium: "250x250#", thumb: "50x50#" }, default_url: "/images/:style/missing.png", :processors => [:cropper]
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
 
   def cropping?
     !crop_x.blank? && !crop_y.blank? && !crop_w.blank? && !crop_h.blank?
+  end
+
+  def avatar_geometry(style = :original)
+    @geometry ||= {}
+    @geometry[style] ||= Paperclip::Geometry.from_file(avatar.path(style))
   end
 
   private
--- lib/paperclip_processors/cropper.rb
+++ lib/paperclip_processors/cropper.rb
@@ -2,8 +2,7 @@ module Paperclip
   class Cropper < Thumbnail
     def transformation_command
       if crop_command
-        puts '2'
-        crop_command + super.first.sub(/ -crop \S+/, '')
+        crop_command << super.join(' ').sub(/-crop \S+/, '')
       else
         super
       end
@@ -12,8 +11,7 @@ module Paperclip
     def crop_command
       target = @attachment.instance
       if target.cropping?
-        puts '3'
-        " -crop '#{target.crop_w}x#{target.crop_h}+#{target.crop_x}+#{target.crop_y}'"
+        " -crop '#{target.crop_w}x#{target.crop_h}+#{target.crop_x}+#{target.crop_y}' "
       end
     end
   end
